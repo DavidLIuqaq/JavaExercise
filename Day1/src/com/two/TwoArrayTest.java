9411344bd84ae762427e6dd6ae27f4daeeb40988
David <786567730@qq.com>
2023-03-09 21:51:37 +0800

    learn

--- Day1/src/com/two/TwoArrayTest.java
+++ Day1/src/com/two/TwoArrayTest.java
@@ -4,5 +4,6 @@ public class TwoArrayTest {
     public static void main(String[] args){
         //声明
         int[][] arr2 = new int[][]{{1,2,3},{1,2,3}};
+        int[][] arr = new int[2][2];
     }
 }
